2299d9588bec7f6c347a982bcd9e21d1d37ad615
adamw <wlosikk.adam@gmail.com>
2023-01-28 15:55:09 +0100

    sample commit message

--- src/AgeChecker.java
+++ src/AgeChecker.java
@@ -3,6 +3,12 @@ import java.util.Scanner;
 public class AgeChecker {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj wiek: ");
+        int age = scanner.nextInt();
+
+        if (age < 18){
+            System.out.println("Wiek mniejszy niz 18");
+        }
 
     }
 }
